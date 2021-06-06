429fd811041632e88494e31f7bc01f12da8631a2
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2021-06-06 15:27:24 +0300

    added option to save pre-filtered list of ids

--- scripts/account_fetch.py
+++ scripts/account_fetch.py
@@ -1,5 +1,5 @@
 # Prep
-import json, configparser, pickle, csv, os
+import json, configparser, pickle, csv
 import math
 import numpy as np
 from tweepy import AppAuthHandler, API, Cursor
@@ -16,7 +16,7 @@ def twitter_auth(config):
 	return api
 
 # Takes given Twitter ID and returns intersection or union of its followers and friends
-def get_connections(account, union = False):
+def get_connections(account, union = False, save_raw = False):
 	flwrs = []
 	frnds = []
 	for flwr in Cursor(api.followers_ids, user_id = account).items():
@@ -27,6 +27,9 @@ def get_connections(account, union = False):
 		connections = list(set(flwrs + frnds))
 	else:
 		connections = list(set(flwrs).intersection(set(frnds)))
+	if save_raw:
+		with open('raw_' + account + '.pickle', 'wb') as outpickle:
+			pickle.dump([frnds, flwrs], outpickle, protocol = 4)
 	return connections
 
 # Checks if any of a set of keywords appears in a user object's bio
@@ -49,9 +52,10 @@ def filter_connections(connections, keywords):
 	return list(filter(None, [check_user(connection, keywords) for connection in filtered]))
 
 #  Wrapper taking list of [account, [keywords]] lists and writing their results to separate csv files
-def process_accounts(acc_info):
+def process_accounts(acc_info, save_raw = False):
 	for account, keywords in acc_info:
-		users = get_connections(account)
+		print('Processing ' + account + '.')
+		users = get_connections(account, save_raw = save_raw)
 		output = filter_connections(users, keywords)
 		np.savetxt('check_' + account + '.csv', output, delimiter = ',', fmt = '% s', encoding = 'utf-8')
 
