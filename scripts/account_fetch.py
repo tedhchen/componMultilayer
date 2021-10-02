e593499491e00daf9f1ebac11553dfd06db44492
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2021-10-03 00:03:41 +0300

    updated code to work with tweepy 4.0.1

--- scripts/account_fetch.py
+++ scripts/account_fetch.py
@@ -13,17 +13,23 @@ def twitter_auth(config, user_auth = False):
 		auth.set_access_token(config['keys']['access'], config['keys']['accesssecret'])
 	else:
 		auth = AppAuthHandler(config['keys']['key'], config['keys']['secret'])
-	api = API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+	api = API(auth, wait_on_rate_limit = True)
 	return api
 
 # Takes given Twitter ID and returns intersection or union of its followers and friends
 def get_connections(account, config, api, union = False, save_raw = False):
 	flwrs = []
 	frnds = []
-	for flwr in Cursor(api.followers_ids, account).items():
-		flwrs.append(flwr)
-	for frnd in Cursor(api.friends_ids, account).items():
-		frnds.append(frnd)
+	if account.isnumeric():
+		for flwr in Cursor(api.get_follower_ids, user_id = account).items():
+			flwrs.append(flwr)
+		for frnd in Cursor(api.get_friend_ids, user_id = account).items():
+			frnds.append(frnd)
+	else:
+		for flwr in Cursor(api.get_follower_ids, screen_name = account).items():
+			flwrs.append(flwr)
+		for frnd in Cursor(api.get_friend_ids, screen_name = account).items():
+			frnds.append(frnd)
 	if union == True:
 		connections = list(set(flwrs + frnds))
 	else:
@@ -49,7 +55,7 @@ def check_user(user, keywords):
 def filter_connections(connections, keywords, api):
 	filtered = []
 	for set100 in np.array_split(connections, math.ceil(len(connections)/100)):
-		filtered.extend(api.lookup_users(list(set100)))
+		filtered.extend(api.lookup_users(user_id = list(set100)))
 	return list(filter(None, [check_user(connection, keywords) for connection in filtered]))
 
 #  Wrapper taking list of [account, [keywords]] lists and writing their results to separate csv files
@@ -65,6 +71,7 @@ def process_accounts(acc_info, config, api, union = False, save_raw = False):
 			output = filter_connections(users, keywords, api)
 			if len(output) > 0:
 				np.savetxt(os.path.join(config['data']['data'], 'check_' + account + '.csv'), output, delimiter = ',', fmt = '% s', encoding = 'utf-8')
+	print('Done!')
 
 # Other related utility functions
 def get_followers(account, save_raw = False):
