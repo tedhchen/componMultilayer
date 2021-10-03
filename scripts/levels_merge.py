3011e819c63fa0c9195bf997300a1b9f711308d1
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2021-10-03 03:41:29 +0300

    updated level merge to work with standardized format

---
+++ scripts/levels_merge.py
@@ -0,0 +1,30 @@
+import csv, configparser, os
+import pandas as pd
+
+def merge_data(path, save = True):
+	df = pd.read_csv(os.path.join(path, 'main_standardized.csv'), dtype = {'id':'str'}, header = 0)
+	mains = [f for f in os.listdir(path) if f[0:6] == 'check_']
+	for main in mains:
+		main_id = main[6:-4]
+		maindat = pd.read_csv(os.path.join(path, main), header = None, dtype = {0:'str', 3:'Int64'}, delimiter = ',', encoding = 'utf-8')
+		maindat = maindat[maindat[3] > 1]
+		if main_id.isnumeric():
+			org = df[df['id'] == main_id].org.item()
+		else:
+			org = df[df['sn'] == main_id].org.item()
+		maindat['org'] = org
+		maindat.rename(columns = {0:'id', 1:'sn', 3:'level'}, inplace = True)
+		df = pd.concat([df, maindat[['org', 'id', 'sn', 'level']]])
+	df.sort_values(by = ['org', 'level', 'id'], axis = 0, inplace = True)
+	df.drop_duplicates(subset = ['org', 'id'], inplace = True)	
+	if save:
+		df.to_csv(os.path.join(path, 'all_accounts.csv'), sep = ',', index = False)
+	return df
+
+# # Example code, uncomment to run:
+# Reading in configuation
+params = configparser.ConfigParser()
+params.read('config.ini')
+
+# Running code
+df = merge_data(path = params['data']['data'])
