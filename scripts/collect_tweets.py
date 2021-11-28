c1636bf5eba12430f6ecfb0300f5f284696f2271
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2021-11-28 06:26:21 +0200

    added basic code to pull 100 user tweets

---
+++ scripts/collect_tweets.py
@@ -0,0 +1,25 @@
+# Prep
+import configparser
+from tweepy import Client, Cursor
+
+def twitter_auth(config, keyname = 'bearer'):
+	client = Client(config['keys'][keyname], wait_on_rate_limit = True)
+	return client
+
+def get_user(user, start_time = '2016-01-01T00:00:00Z', max_results = 100):
+	exps = ['author_id','referenced_tweets.id','attachments.media_keys','attachments.poll_ids','geo.place_id','entities.mentions.username','referenced_tweets.id.author_id']
+	twt_fields = ['attachments','author_id','context_annotations','conversation_id','created_at','entities','geo','in_reply_to_user_id','lang','possibly_sensitive','public_metrics','referenced_tweets','reply_settings','source']
+	usr_fields = ['created_at','description','entities','location','pinned_tweet_id','profile_image_url','protected','public_metrics','url','verified']
+	df = client.search_all_tweets('from:' + str(user), max_results = max_results, start_time = start_time, expansions = exps, tweet_fields = twt_fields, user_fields = usr_fields)
+	return df
+
+# Example code, uncomment to run:
+# Reading in configuation
+params = configparser.ConfigParser(interpolation = None)
+params.read('config.ini')
+
+# Authenticate Twitter API
+client = twitter_auth(params, keyn = 'bearer')
+
+# Get user tweets
+df = get_user('tedhchen')
