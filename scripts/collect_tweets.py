8e03401cd5aa8291ffb670edd01c2c7b5d4b05b1
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2021-11-29 04:54:47 +0200

    added pagination to user data collection

--- scripts/collect_tweets.py
+++ scripts/collect_tweets.py
@@ -1,16 +1,17 @@
 # Prep
 import configparser
-from tweepy import Client, Cursor
+from tweepy import Client, Paginator
 
 def twitter_auth(config, keyname = 'bearer'):
 	client = Client(config['keys'][keyname], wait_on_rate_limit = True)
 	return client
 
-def get_user(user, start_time = '2016-01-01T00:00:00Z', max_results = 100):
+def get_user(user, start_time = '2016-01-01T00:00:00Z', max_results_per_call = 100):
 	exps = ['author_id','referenced_tweets.id','attachments.media_keys','attachments.poll_ids','geo.place_id','entities.mentions.username','referenced_tweets.id.author_id']
 	twt_fields = ['attachments','author_id','context_annotations','conversation_id','created_at','entities','geo','in_reply_to_user_id','lang','possibly_sensitive','public_metrics','referenced_tweets','reply_settings','source']
 	usr_fields = ['created_at','description','entities','location','pinned_tweet_id','profile_image_url','protected','public_metrics','url','verified']
-	df = client.search_all_tweets('from:' + str(user), max_results = max_results, start_time = start_time, expansions = exps, tweet_fields = twt_fields, user_fields = usr_fields)
+	df = [response for response in Paginator(client.search_all_tweets, 'from:' + str(user), start_time = start_time, max_results = max_results_per_call, 
+											 expansions = exps, tweet_fields = twt_fields, user_fields = usr_fields)]
 	return df
 
 # Example code, uncomment to run:
