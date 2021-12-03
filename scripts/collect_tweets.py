e5e740e8c31372ea748d6ba654f36ae89b5437ed
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2021-12-03 08:58:06 +0200

    added function to interface with and collect from all_accounts.csv

--- scripts/collect_tweets.py
+++ scripts/collect_tweets.py
@@ -1,12 +1,13 @@
 # Prep
 import configparser, pickle, os
+import pandas as pd
 from tweepy import Client, Paginator
 
 def twitter_auth(config, keyname = 'bearer'):
 	client = Client(config['keys'][keyname], wait_on_rate_limit = True)
 	return client
 
-def get_user(user, config, return_df = True, save_raw = False, start_time = '2016-01-01T00:00:00Z', max_results_per_call = 100):
+def get_user(user, config, return_df, save_raw, start_time, max_results_per_call = 100):
 	exps = ['author_id','referenced_tweets.id','attachments.media_keys','attachments.poll_ids',
 			'geo.place_id','entities.mentions.username','referenced_tweets.id.author_id']
 	twt_fields = ['attachments','author_id','context_annotations','conversation_id','created_at','entities','geo','in_reply_to_user_id',
@@ -16,12 +17,28 @@ def get_user(user, config, return_df = True, save_raw = False, start_time = '201
 	df = [response for response in Paginator(client.search_all_tweets, 'from:' + str(user), 
 											 start_time = start_time, max_results = max_results_per_call, 
 											 expansions = exps, tweet_fields = twt_fields, user_fields = usr_fields)]
-	if save_raw is not False:
+	if save_raw:
 		os.makedirs(config['data']['rawTweets'], exist_ok = True)
 		with open(os.path.join(config['data']['rawTweets'], 'apiResponse_' + user + '.pickle'), 'wb') as outpickle:
 			pickle.dump(df, outpickle, protocol = 4)
-	if return_df is True:
+	if return_df:
 		return df
+
+def load_accounts(config):
+	df = pd.read_csv(os.path.join(config['data']['scratch'], 'all_accounts.csv'), dtype = {'id':'str'}, header = 0)
+	accs = list(set(df['id']))
+	return accs
+
+def get_accounts(accs, config, ignore_existing = False, start_time = '2017-01-01T00:00:00Z', verbose = True):
+	if ignore_existing:
+		done = [acc[12:-7] for acc in os.listdir(os.path.join(config['data']['rawTweets']))]
+		accs = list(set(accs) - set(done))
+		print('Skipping ' + str(len(done)) + ' accounts already collected. ' + str(len(accs)) + ' accounts remaining.')
+	for acc in accs:
+		if verbose:
+			print('Collecting: ' + str(acc) + '.')
+		get_user(acc, config = config, return_df = False, save_raw = True, start_time = start_time)
+	return None
 
 # Example code, uncomment to run:
 # Reading in configuation
@@ -30,6 +47,13 @@ params.read('config.ini')
 
 # Authenticate Twitter API
 client = twitter_auth(params, keyn = 'bearer')
+
+# Get list of accounts to collect
+accounts = load_accounts(config = params)
+
+accounts = accounts[0:10]
+# Running data collection code
+get_accounts(accounts, config = params, ignore_existing = True, start_time = '2017-01-01T00:00:00Z')
 
 # Get user tweets
 df = get_user('tedhchen', config = params, save_raw = True)
