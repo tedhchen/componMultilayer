8c2bd0dfff059d1f5fe883396d890ffe6b88ab20
Ted Hsuan Yun Chen <ted.hsuanyun.chen@gmail.com>
2025-05-26 03:57:08 -0400

    added option to update tags where already existing

--- scripts/content_addons/add_climate_tags.py
+++ scripts/content_addons/add_climate_tags.py
@@ -7,12 +7,14 @@ params.read('config.ini')
 params.read(params['path']['root'] + 'scripts\config.ini')
 
 # function to add climate tags
-def add_tag(country):
+def add_tag(country, update):
 	print('Processing ' + country + '...')
 	fp = params['data']['componTwitter'] + country + '_edgelist.csv'
 	edf = pd.read_csv(fp, dtype = {'id':'str', 'author_id':'str', 'ref_id':'str', 'ref_author_id':'str', 'created_at':'str', 'text':'str', 'type':'str', 'ref_text':'str'})
 	climate_tags = pd.read_csv(params['data']['tagLocation'] + 'climate_codes95_' + country + '.csv', dtype = {'tw_id': 'str'})
 	climate_tags.rename(columns = {'tw_id': 'id', '95%': 'checked_only95', '95%+all_rest': 'checked_plus95'}, inplace = True)
+	if update:
+		edf.drop(columns=['checked_only95', 'checked_plus95'], inplace = True)
 	edf = pd.merge(edf, climate_tags, how = 'left', left_on = 'id', right_on = 'id')
 	edf['checked_only95'][edf['checked_only95'].isna()] = 0
 	edf['checked_plus95'][edf['checked_plus95'].isna()] = 0
